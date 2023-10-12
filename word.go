ca299334cd758d146149152710e4be61b25514f2
Fabio Vincentelli <fabio.vincentelli@ynov.com>
2023-10-12 12:04:37 +0200

    choix du mot

---
+++ word.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+)
+
+func GetWords() []string {
+	file, err := os.OpenFile("noms_monstres.txt", os.O_RDONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	data, err := os.ReadFile(file.Name())
+
+	slice := []string{}
+	word := ""
+
+	for _, c := range data {
+		if c == '\n' {
+			slice = append(slice, word)
+			word = ""
+		} else {
+			word = word + string(c)
+		}
+	}
+
+	return slice
+}
+
+func GetWord(s []string) string {
+	word := s[rand.Intn(len(s))]
+	return word
+}
+
+func main() {
+	fmt.Print(GetWord(GetWords()))
+}
