eab6aa0ca49fe0e2c67a9fd247c24622721a8814
sromeo <romeo.sanchez@ynov.com>
2023-10-13 08:54:23 +0200

    plein d'erreur mais c'est pour avancer donc trkl

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	fonction.Clear()
+	compteur := 10
+	word := GetWord()
+	free := word[rand.Intn(len(word)-1)]
+	tofind := [len(word)]string{}
+	for i := 0; i < len(word)-1; i++ {
+		if word[i] == free {
+			tofind[i] = word[i]
+		} else {
+			tofind[i] = "_"
+		}
+	}
+
+	for compteur > 0 {
+		Affichage(compteur)
+		fmt.Println(tofind)
+		scan := Scan()
+		for i := 0; i < len(word)-1; i++ {
+			if scan == word[i] {
+				tofind[i] = scan
+			}
+		}
+
+		if scan == word {
+			Win()
+		}
+	}
+}
